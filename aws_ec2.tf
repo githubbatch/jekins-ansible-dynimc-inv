47fded20917babb5864ed1d9ff83d8e55b73b47f
saiteja <139723705+saiteja-0907@users.noreply.github.com>
2023-09-14 10:17:48 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,13 +2,12 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-
 resource "aws_instance" "AWSServer"{
-ami = "ami-02bb7d8191b50f4bb"
+ami = "ami-05552d2dcf89c9b24"
 instance_type = "t2.micro"
-security_groups = ["launch-wizard-2"]
-key_name = "devopsmithuntechnologies"
+security_groups = ["launch-wizard-14"]
+key_name = "redhat2"
 tags = {
-Name =  "tomcatservers"
+Name = "tomcatservers"
 }
 }
